7af3aa21ae45265d57d541ba65db169698f48941
Jaime Hidalgo <jaime.hidalgo.barroso@udla.edu.ec>
2025-09-09 01:10:41 +0000

    par e impar

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Ejemplo: detectar si los números del 1 al 10 son pares o impares
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " es par");
+            } else {
+                System.out.println(i + " es impar");
+            }
+        }
+    }
+}
